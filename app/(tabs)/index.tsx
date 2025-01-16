166cc9e4728a99fb1b069a02361ac1fb75228df2
JaydenRaynes <6.jayray@gmail.com>
2025-01-15 18:13:31 -0600

    Fixed routing and screen displays

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
-export default function TabTwoScreen() {
-  return (
-    <p>Today's Workout</p>
-  );
+import { View, Text } from 'react-native';
+
+export default function explore() {
+    return(
+    <View>
+        <Text>
+            Welcome to the Home Page
+        </Text>
+    </View>)
 }
