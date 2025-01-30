1870d0e98d70ba3ee3f24914643d63d37ff3fc7b
ethan <ethanenniscp0305@gmail.com>
2025-01-30 13:40:05 -0600

    more changes to get map functional

--- app/mapFunctions/nearbyGyms.tsx
+++ app/mapFunctions/nearbyGyms.tsx
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { Gym } from '../models/gymInfoModel';
 
 export const fetchNearbyGyms = async (latitude: number, longitude: number): Promise<Gym[]> => {
+    console.log(latitude);
+    console.log(longitude);
     const apiKey = 'AIzaSyDkm7LX01BhG46JQsWsQQ4RRN2NO9OjmB0'; // Replace with your actual Google API key
     const radius = 5000; // Search within 5 km
     const type = 'gym';
--- app/mapFunctions/userLocation.tsx
+++ app/mapFunctions/userLocation.tsx
@@ -1,24 +1,41 @@
 import { useEffect, useState } from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
 interface Location {
   latitude: number;
   longitude: number;
 }
 
-export const useUserLocation = () => {
-  const [location, setLocation] = useState<Location | null>(null);
+interface UseUserLocationResult {
+    location: Location | null;
+    error: string | null;
+  }
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLocation(position.coords);
-      },
-      (error) => alert(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  }, []);
+export const useUserLocation = (): UseUserLocationResult => {
+    const [location, setLocation] = useState<Location | null>(null);
+    const [error, setError] = useState<string | null>(null);
+  
+    useEffect(() => {
+      const getLocation = () => {
+        Geolocation.getCurrentPosition(
+          (position) => {
+            setLocation({
+            //   latitude: position.coords.latitude,
+            //   longitude: position.coords.longitude,
+            latitude: 32.5232,
+            longitude: 92.6379,
+            });
+          },
+          (err) => {
+            setError(err.message);
+          }
+        );
+      };
 
-  return location;
+      getLocation();
+    }, []);
+
+  return { location, error };
 };
 
 export default useUserLocation;
