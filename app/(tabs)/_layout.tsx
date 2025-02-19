2123ed7482daf02963e819ea14ae1e4265b0cf78
ethan <ethanenniscp0305@gmail.com>
2025-02-19 17:55:31 -0600

    More changes to GymMap

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,70 +7,73 @@ import Entypo from '@expo/vector-icons/Entypo';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="progress"
-        options={{
-          title: 'Progress',
-          tabBarIcon: ({ color }) => <Entypo name="progress-one" size={24} color="black" />,
+    <NavigationContainer>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: Platform.select({
+            ios: {
+              // Use a transparent background on iOS to show the blur effect
+              position: 'absolute',
+            },
+            default: {},
+          }),
         }}
-      />
-      <Tabs.Screen
-        name="exercises"
-        options={{
-          title: 'Exercises',
-          tabBarIcon: ({ color }) => <MaterialIcons name="format-list-numbered" size={24} color="black" />,
-        }}
-      />
-      
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <Feather name="map-pin" size={24} color="black" />,
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Workout',
-          tabBarIcon: ({ color }) => <FontAwesome6 name="house" size={24} color="black" />,
-        }}
-      />
-      <Tabs.Screen
-        name="splits"
-        options={{
-          title: 'Splits',
-          tabBarIcon: ({ color }) => <AntDesign name="calendar" size={24} color="black" />,
-        }}
-      />
-      <Tabs.Screen
-        name="questionare"
-        options={{
-          title: 'Questionare',
-          tabBarIcon: ({ color }) => <FontAwesome6 name="comments" />,
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <Feather name="settings" size={24} color="black" />,
-        }}
-      />   
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="progress"
+          options={{
+            title: 'Progress',
+            tabBarIcon: ({ color }) => <Entypo name="progress-one" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="exercises"
+          options={{
+            title: 'Exercises',
+            tabBarIcon: ({ color }) => <MaterialIcons name="format-list-numbered" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => <Feather name="map-pin" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Workout',
+            tabBarIcon: ({ color }) => <FontAwesome6 name="house" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="splits"
+          options={{
+            title: 'Splits',
+            tabBarIcon: ({ color }) => <AntDesign name="calendar" size={24} color="black" />,
+          }}
+        />
+        <Tabs.Screen
+          name="questionare"
+          options={{
+            title: 'Questionare',
+            tabBarIcon: ({ color }) => <FontAwesome6 name="comments" />,
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Settings',
+            tabBarIcon: ({ color }) => <Feather name="settings" size={24} color="black" />,
+          }}
+        />
+      </Tabs>
+    </NavigationContainer>
   );
 }
