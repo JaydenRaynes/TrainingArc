862b757559f0230b638323ce637db8e5ef52f3c7
JaydenRaynes <6.jayray@gmail.com>
2025-04-27 22:10:56 -0500

    Update generateWorkout.tsx

--- app/(tabs)/generateWorkout.tsx
+++ app/(tabs)/generateWorkout.tsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
+<<<<<<< HEAD
 import { View, Text, ActivityIndicator, ScrollView, StyleSheet, TouchableOpacity, TextInput, Alert, KeyboardAvoidingView, Platform, Modal, FlatList } from "react-native";
 import { fetchUserBiometrics, fetchUserGym } from "../Services/fetchUserData";
+=======
+import { View, Text, ActivityIndicator, ScrollView, StyleSheet, TouchableOpacity, TextInput, Alert, KeyboardAvoidingView, Platform, Modal } from "react-native";
+// import { fetchUserBiometrics, fetchUserGym, fetchUserPreferences } from "../Services/fetchUserData";
+import { fetchUserBiometrics, fetchUserGym } from "../services/fetchUserData";
+//import { Preferences } from "../models/preferenceModel";
+>>>>>>> 8eb1fc3 (Update generateWorkout.tsx)
 import { Gym } from "../models/gymInfoModel";
 import { Biometric } from "../models/biometricModel";
 import { Split, WorkoutDay } from "../models/splitModel";
