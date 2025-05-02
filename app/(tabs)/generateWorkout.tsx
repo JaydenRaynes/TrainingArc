793f102840b462f28a633cb70a995187f12d3842
ethan <ethanenniscp0305@gmail.com>
2025-05-02 13:14:43 -0500

    chenged formatting of edit modal

--- app/(tabs)/generateWorkout.tsx
+++ app/(tabs)/generateWorkout.tsx
@@ -958,7 +958,7 @@ return (
               onRequestClose={() => setSavedModalVisible(false)}
             >
               <View style={styles.modalOverlay}>
-                <View style={styles.modalContainer}>
+                <View style={styles.editModalContainer}>
                   <Text style={styles.modalTitle}>Name Your Workout</Text>
                   <TextInput
                     value={workoutName}
